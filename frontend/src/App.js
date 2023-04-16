ba9e95cd59e12c148d12dec6c5e3a2c9e929c413
Rehan <rbashir2001@gmail.com>
2023-04-16 20:14:10 +0500

    Blog Page Almost Done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,8 @@ import Home from './pages/Home/Home.js';
 import Blog from './pages/Blog/Blog.js';
 import Contact from './pages/Contact/contact.js';
 import Service from './pages/Service/Service.js';
+import BlogPage from './pages/Individual Blog/blogpage.js';
+
 function App() {
   return (
     <BrowserRouter>
@@ -17,6 +19,8 @@ function App() {
           <Route path="/blog" element={<Blog />}></Route>
           <Route path="/contact" element={<Contact />} />
           <Route path="/service" element={<Service />}></Route>
+          <Route path="/blogpagetest" element={<BlogPage />}></Route>
+          {/*Above Test Route Delete Later*/} 
         </Routes>
       </main>
     </BrowserRouter>
