a2e766ac0424cc298af6608a22f0b5d1452825ad
jehanzeb-akram <i200704@nu.edu.pk>
2023-05-17 15:26:49 +0500

    things fixed

--- frontend/src/components/Navbar/MobileNav.js
+++ frontend/src/components/Navbar/MobileNav.js
@@ -158,7 +158,7 @@ export default function MobileNav() {
           </div>
 
           <div className="mobile-navBtn applyOnlineContainer">
-            <Link className="btnLink">
+            <Link className="btnLink" to='/reg'>
               <button className="mobileBtn applyBtn">Apply Online</button>
             </Link>
           </div>
